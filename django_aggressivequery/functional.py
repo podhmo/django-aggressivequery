a291889d07a33c28bedc67188deed684d43bd3d6
podhmo <podhmo+altair@beproud.jp>
2016-07-21 23:14:50 +0900

    for 1.10

--- django_aggressivequery/functional.py
+++ django_aggressivequery/functional.py
@@ -7,8 +7,8 @@ class ExAttributeError(Exception):
 
 
 class cached_property(_cached_property):
-    def __get__(self, instance, type=None):
+    def __get__(self, instance, type_=None):
         try:
-            return super().__get__(instance, type=type)
+            return super().__get__(instance, type_)
         except AttributeError as e:
             raise ExAttributeError(e.args[0])
