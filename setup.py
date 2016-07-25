0013bb222dfbab6ebec9e155ad53d860a8d2d84b
podhmo <podhmo+altair@beproud.jp>
2016-07-25 21:06:28 +0900

    0.3.2

--- setup.py
+++ setup.py
@@ -61,7 +61,7 @@ class MyTest(TestCommand):
 
 
 setup(name='django-aggressivequery',
-      version='0.3.1',
+      version='0.3.2',
       description='handling select_related and prefetch_reated, semi-automatically',
       long_description=README + '\n\n' + CHANGES,
       classifiers=[
